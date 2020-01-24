e684856117be47cc2c599955e0a2b83dbd50698e
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-01-24 09:48:44 -0500

    0.1.2, init index and app files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*
+JOSEPH P. PASAOA
+APP MAIN Component | Portfolio Site
+*/
 
-function App() {
+
+/* IMPORTS */
+    // external
+    import React from 'react';
+
+    // local
+    import './App.css';
+
+
+export default function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello world, Joseph here.
     </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/*
+JOSEPH P. PASAOA
+INDEX Component | Portfolio Site
+*/
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/* IMPORTS */
+    // external
+    import React from 'react';
+    import ReactDOM from 'react-dom';
+    import { BrowserRouter } from 'react-router-dom';
+
+    // local
+    import './index.css';
+    import App from './App';
+
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>, 
+  document.getElementById('root')
+);
