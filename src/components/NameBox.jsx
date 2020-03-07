fef917ef7d04d422397e39cf1d9df4908e091c8e
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-03-07 16:41:17 -0500

    0.2.6, remove animated name, clean nameBox and css

---
+++ src/components/NameBox.jsx
@@ -0,0 +1,30 @@
+/*
+JOSEPH P. PASAOA
+NameBox Component | Portfolio Site
+*/
+
+
+/* IMPORTS */
+import React from 'react';
+
+import './NameBox.css';
+
+
+/* MAIN */
+const NameBox = () => {
+
+  return (
+    <div className="namebox">
+      <h1 className="namebox__name">
+        Joseph P. Pasaoa
+      </h1>
+      <h2 className="namebox__subname">
+        Software Engineer /<br />
+        Full-stack Developer
+      </h2>
+    </div>
+  );
+}
+
+
+export default NameBox;
