94096990202aa890f03bdd0986607c7566571cd1
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-04-05 04:56:00 -0400

    0.14.2, update resume link, add favicon

--- src/helpers/importsRef.js
+++ src/helpers/importsRef.js
@@ -5,8 +5,8 @@ ImportsRef Helper | Portfolio Site
 
 
 /* IMPORTS */
-const joeysResume = process.env.PUBLIC_URL + '/docs/Joseph_P_Pasaoa_Developer_resume.pdf';
-
+// const joeysResume = process.env.PUBLIC_URL + '/docs/Joseph_P_Pasaoa_Developer_resume.pdf';
+const joeysResume = "https://drive.google.com/file/d/1eFhxfPadPQqctHBOEzPz_buJkHb2uoVZ/view?usp=sharing";
 
 /* MAIN */
 module.exports = {
