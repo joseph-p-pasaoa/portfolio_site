b9d66fec00dba94035ccf179e24244d9adcd62f8
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-04-05 22:55:57 -0400

    0.15, implement responsiveness for 769-992px

--- src/pages/HomeAboutPage.jsx
+++ src/pages/HomeAboutPage.jsx
@@ -54,15 +54,15 @@ const HomeAboutPage = () => {
         <div>
           <p className="text para">I studied International Relations and Comparative Politics in college.</p>
 
-          <p className="text para">I've been a freelance photographer and First Assistant to a high-fashion photographer.</p>
+          <p className="text para">I've been a dance, fashion, and portrait photographer, plus First Assistant to a New York City high-fashion photographer.</p>
 
-          <p className="text para">I've had a love for computers my whole life but it wasn't until I was accepted into Pursuit—a selective year-long intensive <strong>full-stack</strong> coding fellowship, that I was able to discover how much I truly enjoy being in the world of software tech.</p>
+          <p className="text para">I've had a love for computers my whole life but it wasn't until I was chosen into <a href="https://www.pursuit.org/" target="_blank" rel="noopener noreferrer" className="link--homepursuit">Pursuit</a> —a selective yearlong intensive <strong>full-stack web</strong> coding fellowship, that I was able to discover how much I truly enjoy being in the world of software tech.</p>
 
-          <p className="text para">My experience in the visual arts and political psychology has been a great well to draw from when working on <strong>UI/UX</strong> and front-ends but I'm just as intrigued, if not more, with the <strong>problem solving</strong> and <strong>structural architecturing</strong> of back-end coding.</p>
+          <p className="text para">My experiences in the visual arts and political psychology have been a great well to draw from when working on <strong>UI/UX</strong> and front-ends however I'm just as intrigued, if not more, with the <strong>problem solving</strong>, <strong>optimization work</strong>, and <strong>structural architecturing</strong> found in both ends.</p>
 
           <p className="text para">I'm looking to find an <strong>rich and diverse environment</strong> where I can grow as an engineer and as a <strong>teammate</strong>.</p>
 
-          <p className="text para">Thanks for visiting!</p>
+          <p className="text para">Thanks for finding out more about me.</p>
         </div>
       </Fade>
 
