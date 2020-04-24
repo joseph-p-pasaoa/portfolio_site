a2074193c92f5bd832b98dfd0b46ad286556c74c
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-04-24 03:51:55 -0400

    0.17, mobile responsiveness pass

--- src/components/NavList.jsx
+++ src/components/NavList.jsx
@@ -5,7 +5,7 @@ NavList Component | Portfolio Site
 
 
 /* IMPORTS */
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { NavLink } from 'react-router-dom';
 import Fade from 'react-reveal/Fade';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -16,6 +16,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 /* MAIN */
 const NavList = () => {
   const [ delayFinished, setDelayFinished ] = useState(false);
+  const [ isDropdownOn, setIsDropdownOn ] = useState(false);
 
   useEffect(() => {
     const delay = () => {
@@ -26,51 +27,85 @@ const NavList = () => {
     delay();
   }, []);
 
+  // SCREEN WIDTH RESPONSE SYSTEM: needed to make navbar dropdown on mobile devices disappear on click
+  const isClient = typeof window === 'object';
+  const checkWidth = useCallback(() => {
+    const screenWidth = isClient ? window.innerWidth : undefined;
+    return screenWidth <= 768;
+  }, [isClient]);
+
+  const [isSmallScreen, setIsSmallScreen] = useState(checkWidth);
+
+  useEffect(() => {
+    if (!isClient) {
+      return false;
+    }
+    function handleResize() {
+      setIsSmallScreen(checkWidth());
+    }
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [isClient, checkWidth]);
+  // END SCREEN WIDTH RESPONSE SYSTEM
+
+  const dropdownToggle = isSmallScreen ? { className: "dropdown-converter" } : false;
+
+
+  const handleClick = () => {
+    setIsDropdownOn(!isDropdownOn);
+  }
+
 
   return (
     <Fade bottom cascade when={delayFinished}>
-      <ul className="navlist">
-        <li>
-          <NavLink exact to={`/`} className="link-navlist">
-            <div>About Me</div>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to={`/projects`} className="link-navlist">
-            <div>Projects</div>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to={`/skills`} className="link-navlist">
-            <div>Skills</div>
-          </NavLink>
-        </li>
-        {/* <li>
-          <NavLink to={`/blog`} className="link-navlist">
-            <div>Blog</div>
-          </NavLink>
-        </li> */}
-        {/* <li>
-          <a href={joeysResume} target="_blank" rel="noopener noreferrer" className="link-navlist">
-            <div>Resume</div>
-          </a>
-        </li> */}
-        <li>
-          <a href="https://www.josephpasaoa.com" target="_blank" rel="noopener noreferrer" className="link-navlist">
-            <div>Photography</div>
-          </a>
-        </li>
-        {/* <li>
-          <NavLink to={`/siteinfo`} className="link-navlist">
-            <div>About This Site</div>
-          </NavLink>
-        </li> */}
-        <li>
-          <NavLink to={`/contactme`} className="link-navlist">
-            <div>Contact Me <FontAwesomeIcon icon={["far", "edit"]} className="faicon--iconbar" /></div>
-          </NavLink>
-        </li>
-      </ul>
+      <div {...dropdownToggle}>
+      {console.log("render")}
+        <button className="dropdown-toggle-btn" onClick={handleClick}>{isDropdownOn ? "- navigate" : "+ navigate"}</button>
+
+        <ul className="navlist" style={{ visibility: isDropdownOn ? "inherit" : "hidden" }}>
+          <li>
+            <NavLink exact to={`/`} className="link-navlist" onClick={handleClick}>
+              <div>About Me</div>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={`/projects`} className="link-navlist" onClick={handleClick}>
+              <div>Projects</div>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={`/skills`} className="link-navlist" onClick={handleClick}>
+              <div>Skills</div>
+            </NavLink>
+          </li>
+          {/* <li>
+            <NavLink to={`/blog`} className="link-navlist">
+              <div>Blog</div>
+            </NavLink>
+          </li> */}
+          {/* <li>
+            <a href={joeysResume} target="_blank" rel="noopener noreferrer" className="link-navlist">
+              <div>Resume</div>
+            </a>
+          </li> */}
+          <li>
+            <a href="https://www.josephpasaoa.com" target="_blank" rel="noopener noreferrer" className="link-navlist" onClick={handleClick}>
+              <div>Photography</div>
+            </a>
+          </li>
+          {/* <li>
+            <NavLink to={`/siteinfo`} className="link-navlist">
+              <div>About This Site</div>
+            </NavLink>
+          </li> */}
+          <li>
+            <NavLink to={`/contactme`} className="link-navlist" onClick={handleClick}>
+              <div>Contact Me <FontAwesomeIcon icon={["far", "edit"]} className="faicon--iconbar" /></div>
+            </NavLink>
+          </li>
+        </ul>
+
+      </div>
     </Fade>
   );
 }
