745b4331fd322df269ad2bbe90e2354513a3ae00
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-05-12 09:30:31 -0400

    edit resume link to pdf from google drive

--- src/helpers/importsRef.js
+++ src/helpers/importsRef.js
@@ -6,7 +6,8 @@ ImportsRef Helper | Portfolio Site
 
 /* IMPORTS */
 // const joeysResume = process.env.PUBLIC_URL + '/docs/Joseph_P_Pasaoa_Developer_resume.pdf';
-const joeysResume = "https://drive.google.com/file/d/1eFhxfPadPQqctHBOEzPz_buJkHb2uoVZ/view?usp=sharing";
+const joeysResume = "https://docs.google.com/document/d/1KzZqXzkyuWH3mDmxMh2TONnUYZOsqbZOQ16UuZg1Am0/export?format=pdf";
+// const joeysResume = "https://drive.google.com/file/d/1eFhxfPadPQqctHBOEzPz_buJkHb2uoVZ/view?usp=sharing";
 
 /* MAIN */
 module.exports = {
