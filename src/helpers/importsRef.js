1202ae6ca15678ace97db6349440a387bd409e04
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-06-09 17:26:07 -0400

    update resume link

--- src/helpers/importsRef.js
+++ src/helpers/importsRef.js
@@ -6,8 +6,8 @@ ImportsRef Helper | Portfolio Site
 
 /* IMPORTS */
 // const joeysResume = process.env.PUBLIC_URL + '/docs/Joseph_P_Pasaoa_Developer_resume.pdf';
-const joeysResume = "https://docs.google.com/document/d/1KzZqXzkyuWH3mDmxMh2TONnUYZOsqbZOQ16UuZg1Am0/export?format=pdf";
-// const joeysResume = "https://drive.google.com/file/d/1eFhxfPadPQqctHBOEzPz_buJkHb2uoVZ/view?usp=sharing";
+const joeysResume = "https://docs.google.com/document/d/1_Q1WSwv2sjGXulK3uKg0dDgCowD7HiLg0KM6MKDqnDA/export?format=pdf";
+
 
 /* MAIN */
 module.exports = {
