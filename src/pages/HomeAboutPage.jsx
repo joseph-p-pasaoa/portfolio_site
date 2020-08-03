8ebabe7925d6efe2a21115be3558547bba96975d
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-08-03 04:55:53 -0400

    update resume

--- src/pages/HomeAboutPage.jsx
+++ src/pages/HomeAboutPage.jsx
@@ -54,11 +54,11 @@ const HomeAboutPage = () => {
         <div className="home__aboutme">
           <p className="text para">I learned BASIC when I was small but I studied International Relations and Comparative Politics in college.</p>
 
-          <p className="text para">I've been a First Assistant to a high-fashion photographer in NYC. I've also been a professional dance, fashion, and portrait photographer, myself.</p>
+          <p className="text para">I've worked hard as a First Assistant to a high-fashion photographer in NYC for a number of years. I've also been a professional dance, fashion, and portrait photographer, myself.</p>
 
-          <p className="text para">Yet it wasn't until I was selected into <a href="https://www.pursuit.org/" target="_blank" rel="noopener noreferrer" className="link--homepursuit">Pursuit</a> —a competitive intensive <strong>full-stack</strong> coding fellowship, that I was able to discover how much I truly enjoy working in software tech.</p>
+          <p className="text para">Yet it wasn't until I was selected into <a href="https://www.pursuit.org/" target="_blank" rel="noopener noreferrer" className="link--homepursuit">Pursuit</a> —an intensive, competitive <strong>full-stack</strong> coding Fellowship, that I was able to discover how much I truly enjoy working on software development.</p>
 
-          <p className="text para">My experiences in the visual arts and political psychology have been a great well to draw from when working on <strong>UI/UX</strong> and front-ends but I'm just as fascinated with the <strong>problem solving</strong>, <strong>optimizing</strong>, and <strong>design process</strong> found across the stack.</p>
+          <p className="text para">My experiences in the visual arts and political psychology have been a great well to draw from when developing front-ends and building <strong>effective UI/UX</strong> but I'm just as fascinated with the <strong>problem solving</strong>, <strong>optimizing</strong>, and <strong>design processes</strong> found all across the stack.</p>
 
           <p className="text para">I'm looking to find an <strong>rich and diverse environment</strong> where I can grow doubly as an engineer and as a <strong>teammate</strong>.</p>
 
