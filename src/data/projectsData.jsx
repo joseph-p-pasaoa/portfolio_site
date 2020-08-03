48f522a1769043d46dee855babc3bcba2bb23d4c
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-08-02 23:54:41 -0400

    init add of Endeavor to projectsData

--- src/data/projectsData.jsx
+++ src/data/projectsData.jsx
@@ -6,7 +6,54 @@ Projects Data | Portfolio Site
 
 export const projectsData = [
   {
-    index: 0,
+    name: "Endeavor",
+    description: `Greenlist is a scalable full-stack registry, directory, and forum application intented to enable fashion designers and brands to find suppliers of eco-friendly green materials, and to facilitate networking within the fashion community for material reclaim.`,
+    screencapUrl: "proj-greenlist-profile.png",
+    urlLive: "https://endeavor-app.herokuapp.com/",
+    urlRepo: "https://github.com/joseph-p-pasaoa/Endeavor",
+    techs: [
+      "PostgreSQL 12.1",
+      "React 16.13.1",
+      "Node.js 12.5.0",
+      "Express.js 4.16.1",
+      "AWS-SDK 2.647.0",
+      "SendGrid API v3 (external)",
+      "Google Calendar (external)",
+      "Multer 1.4.2",
+      "Multer-S3 2.9.0",
+      "Passport 0.3.1",
+      "Passport-Local 1.0.0",
+      "BCrypt 4.0.1",
+      "SendGrid/Mail 7.0.1",
+      "Chart.js 2.9.3",
+      "Moment 2.24.0",
+      "Query-String 6.12.1",
+      "Node-Sass (SCSS) 4.14.1",
+      "React Epic Spinners 0.4.1",
+      "React Icons 3.10.0",
+      "Pg-promise 10.4.4",
+      "Axios 0.19.2",
+      "JavaScript ES5+",
+      "CSS3",
+      "HTML5"
+    ],
+    role: "team project",
+    difficulties: `The assignment for the Uber-sponsored Hackatahon was to create an app to aid in the fashion-sustainability movement, however most in our group hardly knew anything about the fashion industry. With the help of industry advisors, we were able to identify a large gap in the workflow of small and independent designers that we could build something for. Uber developers guided us through dry and performant database query writing and walked us through professional example stages of production and git flow.`,
+    wins: `WE WON Pursuit's Uber-sponsored 6.2 Hackathon's Most Impactful App Award! We reached Minimal Viable Product status in 6 days time by focusing our efforts on the primary role of our app.`,
+    features: [
+      "relational database",
+      "REST API",
+      "server error handling w/ specific http response codes",
+      "front-end & back-end input validation",
+      "intuitive visual feedback",
+      "responsive navbar",
+      "images carousel integration",
+      "multiple-file single operation upload",
+      "conceived and developed to address a real-world problem"
+    ]
+  },
+  //
+  {
     name: "Greenlist",
     description: `Greenlist is a scalable full-stack registry, directory, and forum application intented to enable fashion designers and brands to find suppliers of eco-friendly green materials, and to facilitate networking within the fashion community for material reclaim.`,
     screencapUrl: "proj-greenlist-profile.png",
